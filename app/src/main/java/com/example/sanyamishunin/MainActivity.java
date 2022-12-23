49e4cbd999f332334fcf65e8e428624e51036066
Sanya <hotgun1234@mail.ru>
2022-12-23 19:05:19 +0300

    Конструктор класса

--- app/src/main/java/com/example/sanyamishunin/MainActivity.java
+++ app/src/main/java/com/example/sanyamishunin/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.sanyamishunin;
+//AlexandrMishunin
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,9 +7,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Cat myCat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myCat = new Cat(4, "Puma", "Koshka", "Black");
+        myCat.talk();
+
+        Cat murzik = new Cat(2, "Murzik", "Kot", "White");
+        murzik.talk();
+
+        Cat glasha = new Cat();
+        glasha.age = 1;
+        glasha.name = "Glasha";
+        glasha.talk();
+
     }
 }
